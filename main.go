2ab87def9e2763a7f3dd4b2937aece8adddab90b
Ling Gao <kawoukaka@gmail.com>
2021-06-13 14:02:00 -0400

    add more command parser and utils

--- main.go
+++ main.go
@@ -4,12 +4,18 @@ import (
 	"flag"
 	"fmt"
 	"os"
+
+	utils "github.com/devops-unicorn/buoy/pkg/utils"
 )
 
 func main() {
-	fmt.Println("Hello World!!")
-	// Subcommands
+	// https://github.com/dhirajgidde/Go-Basics/blob/9b144b463e0ab30166393af2e0eb265929a4a22d/Fibonacii/cli/CLI.go
+	// config
 	configCommand := flag.NewFlagSet("config", flag.ExitOnError)
+	componentFlag := configCommand.String("component", "", "component {storage|network|image}. (Required)")
+	durationFlag := configCommand.String("duration", "", "Set scan duration (cron expression)")
+
+	//help
 	helpCommand := flag.NewFlagSet("help", flag.ExitOnError)
 
 	if len(os.Args) < 2 {
@@ -20,8 +26,11 @@ func main() {
 	switch os.Args[1] {
 	case "config":
 		configCommand.Parse(os.Args[2:])
+		fmt.Println("sub-command 'config'")
+		fmt.Println("  component:", *componentFlag)
+		fmt.Println("  duration:", *durationFlag)
 	case "help":
-		break
+		fmt.Println("")
 	default:
 		flag.PrintDefaults()
 		os.Exit(1)
@@ -29,22 +38,26 @@ func main() {
 
 	if configCommand.Parsed() {
 		// Required Flags
-		// if *listTextPtr == "" {
-		//     configCommand.PrintDefaults()
-		//     os.Exit(1)
-		// }
-		// //Choice flag
-		// metricChoices := map[string]bool{"chars": true, "words": true, "lines": true}
-		// if _, validChoice := metricChoices[*listMetricPtr]; !validChoice {
-		//     listCommand.PrintDefaults()
-		//     os.Exit(1)
-		// }
-		// // Print
-		// fmt.Printf("textPtr: %s, metricPtr: %s, uniquePtr: %t\n",iu
-		//     *listTextPtr,
-		//     *listMetricPtr,
-		//     *listUniquePtr,
-		// )
+		if *componentFlag == "" {
+			fmt.Println("Please specify the scanning component.")
+			os.Exit(1)
+		}
+		if *durationFlag == "" {
+			fmt.Println("Please specify cron duration.")
+			os.Exit(1)
+		}
+		componentChoices := map[string]bool{"storage": true, "network": true, "image": true}
+		if _, validChoice := componentChoices[*componentFlag]; !validChoice {
+			configCommand.PrintDefaults()
+			os.Exit(1)
+		}
+		utils.ToTime(*durationFlag)
+		fmt.Printf("You set cron job %q to run\n", *durationFlag)
+
+	}
+
+	if helpCommand.Parsed() {
+		fmt.Println("")
 	}
 
 }
--- pkg/services/scheduler.go
+++ pkg/services/scheduler.go
@@ -1,15 +1,30 @@
 package services
-import "github.com/robfig/cron/v3"
+/*
+Scheduler is using for scheduling scanning component within K8S ecosystem.
+It takes cron expression to schedule tasks. 
+*/
+import (
+	"fmt"
+
+	"github.com/robfig/cron/v3"
+)
 
 //https://pkg.go.dev/github.com/robfig/cron?utm_source=godoc
 
 type Scheduler struct {
+	cronExpression string
+}
+
+func StartScheduler(cronExpression string) {
 	c := cron.New()
 	c.AddFunc("0 30 * * * *", func() { fmt.Println("Every hour on the half hour") })
-	c.start()
+	c.Start()
 
 	// Inspect the cron job entries' next and previous run times.
-	inspect(c.Entries())
+	//inspect(c.Entries())
 
-	c.stop()
+}
+
+func StopScheduler(c *cron.Cron) {
+	c.Stop()
 }
